3d96297b9f8a4af6a460c468c5b49adf7f90a620
shujin <15651026970@163.com>
2018-09-27 08:32:41 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+import VueLazyload from 'vue-lazyload'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI);
+Vue.use(VueAwesomeSwiper);
+Vue.use(VueLazyload);
 
 /* eslint-disable no-new */
 new Vue({
