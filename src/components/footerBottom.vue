25a5cb994a18b7fddf711623306bd0ed8f05acdc
shujin <15651026970@163.com>
2018-09-29 14:49:39 +0800

    1

---
+++ src/components/footerBottom.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="footer-container">{{msg}}</div>
+</template>
+
+<script>
+  export default {
+    name: "footerBottom",
+    data() {
+      return {msg: '底部'}
+    }
+  }
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-container>
+    <el-header>
+      <header-top></header-top>
+    </el-header>
+    <el-main></el-main>
+    <el-footer>
+      <footer-bottom></footer-bottom>
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+  import HeaderTop from "../components/headerTop";
+  import FooterBottom from "../components/footerBottom";
+  export default {
+    name: "index",
+    components: {FooterBottom, HeaderTop},
+    data() {
+      return {}
+    }
+  }
+</script>
